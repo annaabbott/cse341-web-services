04bd0f6459b771b9c75b8b09bdcc625d05b4182a
Anna Abbott <aabbott@byupathway.edu>
2025-10-31 16:19:03 -0500

    completed w01 individual assg

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-// Playing with node without express
-// let http = require('http');
-
-// http.createServer(function (req, res) {
-//     res.writeHead(200, {'Content-Type': 'text/plain'});
-//     res.end('Hello World\n');
-// }).listen(8080);
-
 //init express
 import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 //middleware function to handle requests
-app.use("/", (req, res, next) => res.send("Hello World"));
+
+app.use("/professional", (req, res) =>
+  res.send({
+    professionalName: "Anna Smith",
+    base64Image:
+      "iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAYAAAA+5+5BAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAADl0RVh0U29mdHdhcmUAQWRvYmUgSW1hZ2VSZWFkeXHJZTwAAAXZSURBVHic7d15kFTXefx3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3n3f4P8AqKgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgqCgr6D8A6Kqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqr6H8AoqioKioKioKioKioKioKioKioKioKioKioKioKioKioKioKioKioKioKgP0DgAAsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGxsbGyvgPwDoqurqurqurqurqurqurqurqurqurqurqurqurqurqurqurqugPwCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgr6D8A6Kqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqr6H8AoqioKioKioKioKioKioKioKioKioKioKioKioKioKioKioKioKioKgP0DgAAsbGxsbGxsbGxsbGxsbGxsbGxsbG",
+    nameLink: {
+      firstName: "Anna",
+      url: "https://www.example.com/anna",
+    },
+    primaryDescription:
+      "I am a web developer with a passion for creating dynamic and user-friendly websites.",
+    workDescription1:
+      "I have experience in HTML, CSS, JavaScript, and various web development frameworks.",
+    workDescription2:
+      "I enjoy working on both front-end and back-end development to build complete web applications.",
+    linkTitleText: "Connect with me on:",
+    linkedInLink: {
+      text: "LinkedIn Profile",
+      link: "https://www.linkedin.com/in/anna-smith",
+    },
+    githubLink: {
+      text: "GitHub Profile",
+      link: "",
+    },
+  })
+);
 
 //listen on port
 app.listen(PORT, () => {
--- readme.md
+++ readme.md
@@ -1,7 +1,3 @@
 # Repository Introduction
 
-This repository contains a collection of code for students that take the CSE 341 course at Brigham Young University - Idaho.
-
-## How to use this repository
-
-Each branch contains a collection of code for a given assignment or class activity. Select a branch, and then either clone, fork, or download the zip file to access the files on your computer.
+This repository contains code for projects created as part of the CSE 341 Web Services course at Brigham Young University-Idaho.
